37797cd4267757ee9785a376fd111ee6eca94463
tmle34 <55414823+tmle34@users.noreply.github.com>
2020-11-18 18:49:41 -0600

    Added the change screens functions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Button Adminportal;
+    @FXML
+    Button Clientportal;
+
+
+    final String hostname = "172.26.54.42";
+    final String instance = "CIS-3365*08";
+    final String dbName = "Tri-Tech Informatics Database";
+    final String port = "1433";
+    final String username = "test2";
+    final String password = "yes123";
+    final String AWS_URL = "jdbc:sqlserver://" + hostname + "\\\\" + instance + ":" + port + ";database=" + dbName + ";user=" + username + ";password=" + password;
+
+    public void changeScreenAdmin(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("Adminlog.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+
+    }
+    public void changeScreenClient(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("Clientlog.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 }
